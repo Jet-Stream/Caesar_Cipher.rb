ed72c870cbb9c8309db33b05be0198920117e03e
Tanner <tjohnsto8@gmail.com>
2020-10-05 20:10:12 -0400

    first commit

---
+++ caesar.rb
@@ -0,0 +1,23 @@
+def caesar_cipher(string, shift)
+	the_alphabet = ("a".."z").to_a
+	
+	original_string = string.split('')
+	p original_string
+	coded_string = ''
+	original_string.each do |letter| 
+		if the_alphabet.index(letter) == nil
+			coded_string += letter
+		elsif letter == letter.upcase
+			upcase_index = the_alphabet.index(letter)
+			new_upcase_index = (up_index + shift) % 26
+			coded_string += the_alphabet[new_up_index].upcase
+		else
+		index = the_alphabet.index(letter)
+		new_index = (index + shift) % 26
+		coded_string += the_alphabet[new_index]
+		end
+	end
+		p coded_string
+
+end
+caesar_cipher("Why is there no upcase method??", 1)
